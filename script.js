f80e9f38115e8099d34321d00e66b416e1c72e1e
Pratik Ramteke <pratikramteke222@gmail.com>
2023-02-18 12:51:13 +0530

    dom project 7 added

---
+++ script.js
@@ -0,0 +1,25 @@
+// const language = document.querySelectorAll(".main__languages a");
+
+// for (let i = 0; i < language.length; i++) {
+//   // Do stuff
+//   if (language[i].textContent.includes(2.0)) {
+//     language[i].remove();
+//   }
+// }
+
+// const targetPlaceholder = document.querySelector("input.main__form-input");
+// const form = document.querySelector("form");
+// const submitButton = document.querySelector("button.main__form-btn");
+// targetPlaceholder.placeholder = "iNeuron";
+// targetPlaceholder.removeAttribute("disabled");
+// submitButton.removeAttribute("disabled");
+// form.addEventListener("submit", (e) => {
+//   // e.preventDefault();
+//   let languageSection = document.querySelector(".main__languages");
+//   let createAnchorTag = document.createElement("a");
+//   let createAttribute = document.createAttribute("href");
+//   createAttribute.value = "https://www.ineuron.ai";
+//   createAnchorTag.setAttributeNode(createAttribute);
+//   createAnchorTag.innerText = targetPlaceholder.value;
+//   languageSection.appendChild(createAnchorTag);
+// });
